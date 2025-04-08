62cfa0e45823821504faf89cd21b09a2a6b03d9c
Atharva7887 <athshirke2001@gmail.com>
2025-04-08 11:10:22 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String main[])
+    {
+       
+        int a = 10;
+        int b = 2;
+        System.out.println("Here is the division: "+(a/b));
+        System.out.println("Here is the modulus: "+(a%b));
+    }
+}
